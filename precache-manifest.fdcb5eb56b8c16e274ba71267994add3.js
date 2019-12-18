ffdd879990be299350e4d35880ee4e62849167b5
maxshashoua@gmail.com <maxshashoua@gmail.com>
2019-12-18 17:37:52 -0500

    Updates

---
+++ precache-manifest.fdcb5eb56b8c16e274ba71267994add3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42063c1a19df366667bdc74b293f3826",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "71743eb9318791d8e312",
+    "url": "/portfolio-site/static/css/main.ba12e587.chunk.css"
+  },
+  {
+    "revision": "652e627e16eb87b7863e",
+    "url": "/portfolio-site/static/js/2.22535751.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/portfolio-site/static/js/2.22535751.chunk.js.LICENSE"
+  },
+  {
+    "revision": "71743eb9318791d8e312",
+    "url": "/portfolio-site/static/js/main.9ae26ac1.chunk.js"
+  },
+  {
+    "revision": "08bfe5b81e7a980d1d3a",
+    "url": "/portfolio-site/static/js/runtime-main.5694b0f3.js"
+  },
+  {
+    "revision": "ec42c9fe430070e6c33a84b1098ec7f4",
+    "url": "/portfolio-site/static/media/moic-photo.ec42c9fe.png"
+  }
+]);
